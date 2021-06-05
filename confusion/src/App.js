f5504178f3cd82ce03d418de49715479aa08ce6f
Jason Le <jasonctle@gmail.com>
2021-06-04 18:31:24 -0700

    added route for signup page

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./components/layout/Navigation.js";
 import Waitlist from "./components/waitlist/Waitlist";
 import Login from "./components/waitlist/Login";
+import Signup from "./components/waitlist/Signup";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Waitlist />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
