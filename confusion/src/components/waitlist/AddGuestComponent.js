0d95a297b47cfafb20cd3f15ebb072734f21f725
Jason Le <jasonctle@gmail.com>
2021-06-09 20:11:30 -0700

    Added add guest component

---
+++ confusion/src/components/waitlist/AddGuestComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AddGuestComponent = () => {
+  return (
+    <div className="container">
+      <h1>Business</h1>
+      <h2>Join Waitlist now!</h2>
+      <h6>Select your party size:</h6>
+      <h6>Your information</h6>
+    </div>
+  );
+};
+
+export default AddGuestComponent;
