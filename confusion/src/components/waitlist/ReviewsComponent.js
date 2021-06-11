ef94af1fc8a53533d2d02b2d2609475954026c97
Jason Le <jasonctle@gmail.com>
2021-06-10 20:13:16 -0700

    Added Leave review component

---
+++ confusion/src/components/waitlist/ReviewsComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReviewsComponent = () => {
+  return (
+    <div className="container">
+      <h1>Review</h1>
+    </div>
+  );
+};
+
+export default ReviewsComponent;
