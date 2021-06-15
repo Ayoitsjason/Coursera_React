9be47f00507ab8cdff1f125733390d90c7bee3d6
Jason Le <jasonctle@gmail.com>
2021-06-14 23:08:06 -0700

    fixed react links inside navbar

--- confusion/src/components/layout/Navigation.js
+++ confusion/src/components/layout/Navigation.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 const Navigation = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <LinkContainer to="/">
-        <Navbar.Brand className="mx-5">Tippin</Navbar.Brand>
-      </LinkContainer>
+      <Navbar.Brand as={Link} to="/" className="mx-5">
+        Tippin
+      </Navbar.Brand>
       <Navbar.Toggle className="mx-5" aria-controls="basic-navbar-nav" />
       <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
         <Nav className="mx-5" activeKey="/home">
           <Nav.Item>
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
             <Nav.Link href="/signup">Signup</Nav.Link>
