f94a715129f280446d2e4cf86d238966457e946d
Jason Le <jasonctle@gmail.com>
2021-06-19 23:44:40 -0700

    added form to login

--- confusion/src/components/waitlist/Login.js
+++ confusion/src/components/waitlist/Login.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const Login = () => {
-  return <div className="container">Login</div>;
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <Formik>
+        {(props) => (
+          <Form>
+            <fieldset className="form-group">
+              <label>Username</label>
+              <Field className="form-control" type="text" name="description" />
+            </fieldset>
+            <fieldset className="form-group">
+              <label>Password</label>
+              <Field className="form-control" type="text" name="description" />
+            </fieldset>
+            <button className="btn btn-success" type="submit">
+              Login
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
 
 export default Login;
