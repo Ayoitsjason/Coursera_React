b319d30dc853c59038aed950d4b5213c161693fe
Jason Le <jasonctle@gmail.com>
2021-06-21 13:48:18 -0700

    added initial value to formik in signup page

--- confusion/src/components/waitlist/Signup.js
+++ confusion/src/components/waitlist/Signup.js
@@ -2,31 +2,48 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const Signup = () => {
+  const onSubmit = (props) => {
+    console.log(props);
+  };
+
   return (
     <div className="container">
       <h1>Register</h1>
-      <Formik>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          password: "",
+          password2: "",
+          email: "",
+        }}
+        validateOnChange={false}
+        validateOnBlur={false}
+        onSubmit={onSubmit}
+        // validate={this.validate}
+        enableReinitialize={true}
+      >
         {(props) => (
           <Form>
             <fieldset className="form-group">
               <label>First Name</label>
-              <Field className="form-control" type="text" name="description" />
+              <Field className="form-control" type="text" name="firstName" />
             </fieldset>
             <fieldset className="form-group">
               <label>Last Name</label>
-              <Field className="form-control" type="text" name="description" />
+              <Field className="form-control" type="text" name="lastName" />
             </fieldset>
             <fieldset className="form-group">
               <label>Password</label>
-              <Field className="form-control" type="text" name="description" />
+              <Field className="form-control" type="text" name="password" />
             </fieldset>
             <fieldset className="form-group">
               <label>Confirm Password</label>
-              <Field className="form-control" type="text" name="description" />
+              <Field className="form-control" type="text" name="password2" />
             </fieldset>
             <fieldset className="form-group">
               <label>Email</label>
-              <Field className="form-control" type="text" name="description" />
+              <Field className="form-control" type="text" name="email" />
             </fieldset>
             <button className="btn btn-success" type="submit">
               Register
