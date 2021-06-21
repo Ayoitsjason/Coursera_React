3649c5cbc66fba2ea30871f788bf2b0af08678c3
Jason Le <jasonctle@gmail.com>
2021-06-21 13:45:23 -0700

    added formik to login page with initial state

--- confusion/src/components/waitlist/Login.js
+++ confusion/src/components/waitlist/Login.js
@@ -2,19 +2,30 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const Login = () => {
+  const onSubmit = (props) => {
+    console.log(props);
+  };
+
   return (
     <div className="container">
       <h1>Login</h1>
-      <Formik>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validateOnChange={false}
+        validateOnBlur={false}
+        onSubmit={onSubmit}
+        // validate={this.validate}
+        enableReinitialize={true}
+      >
         {(props) => (
           <Form>
             <fieldset className="form-group">
               <label>Username</label>
-              <Field className="form-control" type="text" name="description" />
+              <Field className="form-control" type="text" name="username" />
             </fieldset>
             <fieldset className="form-group">
               <label>Password</label>
-              <Field className="form-control" type="text" name="description" />
+              <Field className="form-control" type="text" name="password" />
             </fieldset>
             <button className="btn btn-success" type="submit">
               Login
