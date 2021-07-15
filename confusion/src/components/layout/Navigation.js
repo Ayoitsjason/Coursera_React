0d11b9fed7548efabbe25b402982042914f9d27b
Jason Le <jasonctle@gmail.com>
2021-07-14 18:36:58 -0700

    Refactored navigation code

--- confusion/src/components/layout/Navigation.js
+++ confusion/src/components/layout/Navigation.js
@@ -5,57 +5,26 @@ import "./Navigation.css";
 
 const Navigation = (props) => {
   return (
-    <>
-      {props.loggedIn ? (
-        <Navbar className="d-none" bg="light" expand="lg">
-          <Navbar.Brand as={Link} to="/" className="mx-5">
-            Tippin
-          </Navbar.Brand>
-          <Navbar.Toggle className="mx-5" aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            className="justify-content-end"
-            id="basic-navbar-nav"
-          >
-            <Nav className="mx-5" activeKey="/home">
-              <Nav.Item>
-                <Nav.Link as={Link} to="/login">
-                  Login
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link as={Link} to="/signup">
-                  Signup
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      ) : (
-        <Navbar expand="lg">
-          <Navbar.Brand as={Link} to="/" className="mx-5">
-            Tippin
-          </Navbar.Brand>
-          <Navbar.Toggle className="mx-5" aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            className="justify-content-end"
-            id="basic-navbar-nav"
-          >
-            <Nav className="mx-5" activeKey="/home">
-              <Nav.Item>
-                <Nav.Link as={Link} to="/login">
-                  Login
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link as={Link} to="/signup">
-                  Signup
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      )}
-    </>
+    <Navbar className={props.loggedIn ? "d-none" : ""} expand="lg">
+      <Navbar.Brand as={Link} to="/" className="mx-5">
+        Tippin
+      </Navbar.Brand>
+      <Navbar.Toggle className="mx-5" aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+        <Nav className="mx-5" activeKey="/home">
+          <Nav.Item>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/signup">
+              Signup
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
