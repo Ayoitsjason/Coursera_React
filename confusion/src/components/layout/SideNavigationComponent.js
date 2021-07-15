8567a2341bfbafc31685a6796be6d1f221e673c6
Jason Le <jasonctle@gmail.com>
2021-07-14 18:29:21 -0700

    Adjusted side nav for better mobile experience

--- confusion/src/components/layout/SideNavigationComponent.js
+++ confusion/src/components/layout/SideNavigationComponent.js
@@ -10,31 +10,32 @@ const SideNavigationComponent = (props) => {
     props.updateAuth();
   };
   return (
-    <div className="side-navigation">
-      <Navbar className="flex-column" expand="lg">
-        <Navbar.Brand className="mx-5" as={Link} to="/">
-          Tippin
-        </Navbar.Brand>
-        <Navbar.Toggle className="mx-5" aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-          <Nav className="mx-5 flex-column" activeKey="/home">
-            <Nav.Item>
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as={Link} to="/reviews">
-                Reviews
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link onClick={onClickLogout}>Logout</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+    <Navbar
+      className="justify-content-center side-navigation flex-column"
+      expand="lg"
+    >
+      <Navbar.Brand className="m-0" as={Link} to="/">
+        Tippin
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="flex-column" activeKey="/home">
+          <Nav.Item>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/reviews">
+              Reviews
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={onClickLogout}>Logout</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
