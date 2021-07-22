77f14324f18b5aa154ccfe656b78847e0cc8e38f
Jason Le <jasonctle@gmail.com>
2021-07-22 15:55:49 -0700

    Changed button in leave review component to radio buttons

--- confusion/src/components/waitlist/LeaveReviewComponent.js
+++ confusion/src/components/waitlist/LeaveReviewComponent.js
@@ -1,8 +1,18 @@
-import React from "react";
-import { Button, Col, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Col, Row, ToggleButton, ButtonGroup } from "react-bootstrap";
 import SideNavigationComponent from "../layout/SideNavigationComponent";
 
 const LeaveReviewComponent = (props) => {
+  const [checked, setChecked] = useState(false);
+  const [radioValue, setRadioValue] = useState("1");
+
+  const radios = [
+    { name: "Active", value: "1" },
+    { name: "Radio", value: "2" },
+    { name: "Radio", value: "3" },
+    { name: "Radio", value: "4" },
+    { name: "Radio", value: "5" },
+  ];
   return (
     <div className="App">
       <Row className="m-0">
@@ -15,11 +25,22 @@ const LeaveReviewComponent = (props) => {
           <h1 className="my-5">Leave a Review!</h1>
           <div className="container">
             <div>
-              <Button className="review__components-svg1"></Button>
-              <Button className="review__components-svg2"></Button>
-              <Button className="review__components-svg3"></Button>
-              <Button className="review__components-svg4"></Button>
-              <Button className="review__components-svg5"></Button>
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={"outline-light"}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    <img className={`review__components-svg${radio.value}`} />
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
             </div>
             <div>
               <input />
