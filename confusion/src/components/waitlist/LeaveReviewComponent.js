1d7a34d945f14efa73746b4473c7ebd806ed373f
Jason Le <jasonctle@gmail.com>
2021-07-23 18:22:14 -0700

    removed border from svgs in leave a review component

--- confusion/src/components/waitlist/LeaveReviewComponent.js
+++ confusion/src/components/waitlist/LeaveReviewComponent.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
-import { Button, Col, Row, ToggleButton, ButtonGroup } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Row,
+  ToggleButton,
+  ButtonGroup,
+  Form,
+} from "react-bootstrap";
 import SideNavigationComponent from "../layout/SideNavigationComponent";
 
 const LeaveReviewComponent = (props) => {
-  const [checked, setChecked] = useState(false);
   const [radioValue, setRadioValue] = useState("1");
 
   const radios = [
-    { name: "Active", value: "1" },
-    { name: "Radio", value: "2" },
-    { name: "Radio", value: "3" },
-    { name: "Radio", value: "4" },
-    { name: "Radio", value: "5" },
+    { value: "1" },
+    { value: "2" },
+    { value: "3" },
+    { value: "4" },
+    { value: "5" },
   ];
+
+  const onClickSubmit = (e) => {
+    console.log(e);
+  };
+
   return (
     <div className="App">
       <Row className="m-0">
@@ -24,28 +35,35 @@ const LeaveReviewComponent = (props) => {
         <Col xs={9}>
           <h1 className="my-5">Leave a Review!</h1>
           <div className="container">
-            <div>
+            <form>
               <ButtonGroup>
                 {radios.map((radio, idx) => (
                   <ToggleButton
                     key={idx}
                     id={`radio-${idx}`}
+                    className={`review__components review__components-svg${radio.value} rounded-circle`}
                     type="radio"
-                    variant={"outline-light"}
+                    variant={"light"}
                     name="radio"
                     value={radio.value}
                     checked={radioValue === radio.value}
-                    onChange={(e) => setRadioValue(e.currentTarget.value)}
-                  >
-                    <img className={`review__components-svg${radio.value}`} />
-                  </ToggleButton>
+                    onChange={(e) => {
+                      console.log(e);
+                      setRadioValue(e.currentTarget.value);
+                    }}
+                  ></ToggleButton>
                 ))}
               </ButtonGroup>
-            </div>
-            <div>
-              <input />
-            </div>
-            <Button className="btn btn-primary">Submit</Button>
+              <div>
+                <input />
+              </div>
+              <Button
+                className="btn btn-primary"
+                onClick={(e) => onClickSubmit(e)}
+              >
+                Submit
+              </Button>
+            </form>
           </div>
         </Col>
       </Row>
