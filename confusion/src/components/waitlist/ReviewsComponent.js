d34ed258fd055eed74ccee34056470a4b2aad723
Jason Le <jasonctle@gmail.com>
2021-10-21 23:51:47 -0700

    Started initial code in reviews component

--- confusion/src/components/waitlist/ReviewsComponent.js
+++ confusion/src/components/waitlist/ReviewsComponent.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import SideNavigationComponent from "../layout/SideNavigationComponent";
 
 const ReviewsComponent = (props) => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+
+  })
+
+  const populateReviews = (review) => {
+    return (<div>review</div>);
+  };
+
   return (
     <div className="App">
       <Row className="m-0">
@@ -11,6 +20,11 @@ const ReviewsComponent = (props) => {
         ) : null}
         <Col xs={props.isLoggedIn ? "9" : "12"}>
           <h1 className="my-5">Reviews</h1>
+          {reviews.length > 0
+          ? reviews.map((review) =>
+              populateReviews(review)
+            )
+          : null}
         </Col>
       </Row>
     </div>
