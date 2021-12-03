b512f560de55bdb7190f4c208e7078e68c8b4814
Jason Le <jasonctle@gmail.com>
2021-12-03 11:25:13 -0800

    Added form validation for sign up component, still working on unmatched password

--- confusion/src/components/waitlist/Signup.js
+++ confusion/src/components/waitlist/Signup.js
@@ -1,11 +1,53 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { Alert } from "react-bootstrap";
 import { RegisterOwner } from "../api/WaitlistDataService";
 
 const Signup = () => {
-  const onSubmit = (props) => {
-    RegisterOwner(props);
+  const onSubmit = (values) => {
+    let error = confirmPassword(values.password, values.password2);
+    console.log(error);
+    if (error.length < 1) {
+      console.log(values);
+    } else {
+      return error;
+    }
   };
+
+  const SignUpSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .max(20, "Too Long! Max length 20 characters")
+      .required("Required"),
+    lastName: Yup.string()
+      .max(20, "Too Long! Max length 20 charasters")
+      .required("Required"),
+    username: Yup.string()
+      .min(6, "Too Short! Min Length 6 characters")
+      .max(20, "Too Long! Max Length 20 characters")
+      .required("Required"),
+    password: Yup.string()
+      .min(6, "Too Short! Min Length 6 characters")
+      .max(30, "Too Long! Max Length 30 characters")
+      .required("Required"),
+    password2: Yup.string()
+      .min(6, "Too Short! Min Length 6 characters")
+      .max(30, "Too Long! Max Length 30 characters")
+      .required("Required"),
+    email: Yup.string().required("Required"),
+    businessName: Yup.string()
+      .max(30, "Too long! Max Length 30 characters")
+      .required("Required"),
+  });
+
+  function confirmPassword(password, password2) {
+    let error = "";
+    console.log(password, password2);
+    if (password !== password2) {
+      error = "Passwords do not match!";
+    }
+    return error;
+  }
 
   return (
     <div className="container">
@@ -22,39 +64,74 @@ const Signup = () => {
         }}
         validateOnChange={false}
         validateOnBlur={false}
-        onSubmit={onSubmit}
-        // validate={this.validate}
+        onSubmit={(values, { resetForm }) => onSubmit(values, resetForm)}
+        validationSchema={SignUpSchema}
         enableReinitialize={true}
       >
-        {(props) => (
+        {({ errors, touched }) => (
           <Form>
             <fieldset className="form-group">
               <label>First Name</label>
               <Field className="form-control" type="text" name="firstName" />
+              {errors.firstName && touched.firstName ? (
+                <Alert key="warning" variant="warning">
+                  {errors.firstName}
+                </Alert>
+              ) : null}
             </fieldset>
             <fieldset className="form-group">
               <label>Last Name</label>
               <Field className="form-control" type="text" name="lastName" />
+              {errors.lastName && touched.lastName ? (
+                <Alert key="warning" variant="warning">
+                  {errors.lastName}
+                </Alert>
+              ) : null}
             </fieldset>
             <fieldset className="form-group">
               <label>Username</label>
               <Field className="form-control" type="text" name="username" />
+              {errors.username && touched.username ? (
+                <Alert key="warning" variant="warning">
+                  {errors.username}
+                </Alert>
+              ) : null}
             </fieldset>
             <fieldset className="form-group">
               <label>Password</label>
               <Field className="form-control" type="text" name="password" />
+              {errors.password && touched.password ? (
+                <Alert key="warning" variant="warning">
+                  {errors.password}
+                </Alert>
+              ) : null}
             </fieldset>
             <fieldset className="form-group">
               <label>Confirm Password</label>
               <Field className="form-control" type="text" name="password2" />
+              {errors.password2 && touched.password2 ? (
+                <Alert key="warning" variant="warning">
+                  {errors.password2}
+                </Alert>
+              ) : null}
             </fieldset>
             <fieldset className="form-group">
               <label>Email</label>
               <Field className="form-control" type="text" name="email" />
+              {errors.email && touched.email ? (
+                <Alert key="warning" variant="warning">
+                  {errors.email}
+                </Alert>
+              ) : null}
             </fieldset>
             <fieldset className="form-group">
               <label>Business Name</label>
               <Field className="form-control" type="text" name="businessName" />
+              {errors.businessName && touched.businessName ? (
+                <Alert key="warning" variant="warning">
+                  {errors.businessName}
+                </Alert>
+              ) : null}
             </fieldset>
             <button className="btn btn-primary" type="submit">
               Register
